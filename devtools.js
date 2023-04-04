4540a9e9188118cdd93cae6275e5f47ffebee2d7
Adam Oberle <oberle.adam@gmail.com>
2023-04-04 09:07:42 -0500

    DevTools video follow

--- devtools.js
+++ devtools.js
@@ -1,1 +1,14 @@
-console.log( 'js' )
+// Using the DevTools Console
+
+let firstNumber = 1;
+let secondNumber = 2;
+
+let answer = firstNumber + secondNumber;
+
+// console.log()
+console.log( answer );  // comes up with answer of 3
+
+console.log( 'the answer is ' + answer );
+
+console.log( 'the answer is', answer );
+// , makes number appear as number in console rather than string
